7cbb3843773fa849876d8503e9c78b1f085ba62b
Dwarak-10 <dwarakeshvaran0512@gmail.com>
2024-11-21 20:31:16 +0530

    final

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
+import { Link } from "react-scroll";
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
@@ -38,7 +39,10 @@ const Navbar = () => {
             key={id}
             className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200"
           >
-            {link}
+            <Link to={link} smooth duration={500}>
+              {" "}
+              {link}
+            </Link>
           </li>
         ))}
       </ul>
@@ -56,7 +60,15 @@ const Navbar = () => {
               key={id}
               className="capitalize text-4xl px-4 py-6 cursor-pointer"
             >
-              {link}
+              <Link
+                to={link}
+                smooth
+                duration={500}
+                onClick={() => setNav(!nav)}
+              >
+                {" "}
+                {link}
+              </Link>{" "}
             </li>
           ))}
         </ul>
